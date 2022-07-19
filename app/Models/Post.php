90d445d901922e41a79973cf84bc16360a4597c8
Denta Muhajir <dentamuhajir@gmail.com>
2022-07-19 18:44:19 +0700

    #6 :create model tag & relationship with model post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,4 +14,9 @@ class Post extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function tag()
+    {
+        return $this->belongsToMany(Tag::class, 'post_tags');
+    }
 }
